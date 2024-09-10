2e820ca3d04cfc307aacf71967ba75e05c37c5a6
Himanshu Hardiya <himanshuhardiya18@gmail.com>
2024-09-10 10:25:38 +0530

    store setup and theme function done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import ViiTubeTheme from './utils/ViiTubeTheme'
 
-function App() {
+function App({darkMode, onToggleTheme}) {
 
   return (
-    <>
-      
-    </>
+    <div className={`${darkMode?"bg-green-500":"bg-pink-800"} `}>
+      <br />
+        <button onClick={onToggleTheme}>click</button>
+    </div>
   )
 }
 
-export default App
+export default ViiTubeTheme(App)
---
+++ frontend/src/utils/ViiTubeTheme.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { toggleTheme } from '../store/themeSlice'
+
+const ViiTubeTheme = (WrappedComponent) => {
+
+    return (props)=>{
+        const darkMode = useSelector((state)=>state.theme.darkMode)
+        const dispatch = useDispatch()
+
+        const handleTheme = ()=>{
+            dispatch(toggleTheme())
+        }
+
+        return (
+            <div>
+              <WrappedComponent {...props} darkMode={darkMode} onToggleTheme={handleTheme} />
+            </div>
+        )
+    }
+}
+
+export default ViiTubeTheme 
